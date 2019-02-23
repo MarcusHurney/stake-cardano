2888a7ae783a4c5dba7fe4875fd3e9374aa77f7c
MarcusHurney <marcushurney@gmail.com>
2019-02-23 04:41:51 -0800

    Adds onresize hook to Space.js and passes window data to Block.js

--- src/components/Space/Block.js
+++ src/components/Space/Block.js
@@ -14,5 +14,10 @@ export const Block = (props: Props) => {
     innerHeight: ${innerHeight},
     innerWidth: ${innerWidth}
   `);
+  // TODO
+  // 1. check if block has run into innerHeight or innerWidth
+  // 2. if it has then change it's orientation angle and speed to bounce it in the opposite direction
+  // 3. look at pong for implementation detials
+  // 4. burn block if it hits the cloud?
   return <img src={block} className={styles['block']} alt="block" />;
 }
--- src/components/Space/Space.js
+++ src/components/Space/Space.js
@@ -1,13 +1,23 @@
-import React, { Component } from 'react';
+// @flow
+import React, { useState, useEffect } from 'react';
+import { debounce } from 'lodash';
 import { Block } from './Block';
 import styles from './Space.module.scss';
 
-export class Space extends Component {
-  render() {
-    return (
-      <div className={styles['space']}>
-        <Block />
-      </div>
-    );
-  }
+export const Space = () => {
+  const { innerHeight, innerWidth } = window;
+  const [windowDimensions, setDimensions] = useState({ innerHeight, innerWidth });
+  const handleResize = () => setDimensions({ innerHeight, innerWidth });
+  const debouncedResize = debounce(handleResize, 300);
+
+  useEffect(() => {
+    window.addEventListener('resize', debouncedResize);
+    return () => window.removeEventListener('resize', debouncedResize);
+  }, [windowDimensions]);
+
+  return (
+    <div className={styles['space']}>
+      <Block { ...windowDimensions } />
+    </div>
+  );
 }
