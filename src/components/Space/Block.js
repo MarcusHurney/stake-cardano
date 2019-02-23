b6e6c9a3050a3fa1ca39a995c8aa3adaf1dc941a
MarcusHurney <marcushurney@gmail.com>
2019-02-23 03:35:00 -0800

    Sets up Flow configuration

--- src/components/Space/Block.js
+++ src/components/Space/Block.js
@@ -1,7 +1,18 @@
+// @flow
 import React from 'react';
 import block from '../../assets/svg/block.svg';
 import styles from './Block.module.scss';
 
-export const Block = () => (
-  <img src={block} className={styles['block']} alt="block" />
-);
+type Props = {
+  innerHeight: number,
+  innerWidth: number,
+};
+
+export const Block = (props: Props) => {
+  const { innerHeight, innerWidth } = props;
+  console.log(`
+    innerHeight: ${innerHeight},
+    innerWidth: ${innerWidth}
+  `);
+  return <img src={block} className={styles['block']} alt="block" />;
+}
--- src/components/Space/Space.js
+++ src/components/Space/Space.js
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Block } from './Block';
 import styles from './Space.module.scss';
 
-export const Space = () => (
-  <div className={styles['space']}>
-    <Block />
-  </div>
-);
+export class Space extends Component {
+  render() {
+    return (
+      <div className={styles['space']}>
+        <Block />
+      </div>
+    );
+  }
+}
