ffb56b8fb288408989bb663e9011fc7822be74c7
MarcusHurney <marcushurney@gmail.com>
2019-03-20 15:11:31 -1000

    Separates deep_space section of website into two distinct halves

--- src/App.js
+++ src/App.js
@@ -3,18 +3,24 @@ import { Banner } from './components/Banner/Banner';
 import { Logo } from './components/Logo/Logo';
 import { Navbar } from './components/Navbar/Navbar';
 import { Space } from './components/Space/Space';
-import styles from './App.module.scss';
+import appStyles from './App.module.scss';
+import logoStyles from './components/Logo/Logo.module.scss';
 
 class App extends Component {
   render() {
     return (
-      <div className={styles['app']}>
+      <div className={appStyles['app']}>
         <Navbar />
-        <div className={styles['app__content']}>
-          <Logo />
-          <Space>
-            <Banner />
-          </Space>
+        <div className={appStyles['deep_space_wrapper']}>
+          <div className={appStyles['deep_space_visuals']}>
+            <Logo styles={logoStyles} />
+            <Space>
+              <Banner />
+            </Space>
+          </div>
+          <div className={appStyles['deep_space_content']}>
+            <h1>Let's Decentralize Cardano</h1>
+          </div>
         </div>
       </div>
     );
