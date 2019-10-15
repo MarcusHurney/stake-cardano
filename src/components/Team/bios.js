af25b024697b1981ed9bbc168c62e745c8675c94
MarcusHurney <marcushurney@gmail.com>
2019-10-14 23:33:06 -0400

    Adds Marcus's bio

--- src/components/Team/bios.js
+++ src/components/Team/bios.js
@@ -6,9 +6,9 @@ export const teamBios = {
   He also has extensive experience in distribution and logistics. He started investing in cryptocurrency in May of 2017.
  `,
  marcusBio: `
-  Philippe graduated from Columbia University with a B.A. in Environmental Biology.
-  He is currently the CEO of a government contracting firm that specializes in facility management/service contracts.
-  He also has extensive experience in distribution and logistics. He started investing in cryptocurrency in May of 2017.
+  Marcus graduated from The University of Georgia with a B.A. in Comparative Literature.
+  He is currently a frontend develop working on Daedalus at IOHK and maintains the open source library react-polymorph.
+  Marcus has five years of experience building scalable and performant web applications for early stage and midsize startups.
  `,
  jeremyBio: `
   Jeremy graduated from Coleman College with a B.S. in Computer Applications & Networking while serving in the U.S. Navy as a Network Engineer/Server Administrator.
